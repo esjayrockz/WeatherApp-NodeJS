d12dcd93985bd51e20872bed8d64da8b929c8870
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-19 22:20:57 -0400

    Removing unnecessary files and making final product

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -15,19 +14,35 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocode(argv.a, (errorMessage, results) => {
-  if(errorMessage){
-    console.log(errorMessage);
+if(argv.address.length<2){
+  console.log('Please enter valid address');
+  yargs.showHelp();
+  process.exit(0);
+}
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+const geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if(response.data.status === 'ZERO_RESULTS'){
+    throw new Error('Unable to find that address');
+  }
+  const address = response.data.results[0].formatted_address;
+  const lattitude = response.data.results[0].geometry.location.lat;
+  const longitude = response.data.results[0].geometry.location.lng;
+  console.log('Address:', address);
+  const weatherUrl = `https://api.darksky.net/forecast/9a3deb85a259ca7e203c999c83050c45/${lattitude},${longitude}`;
+  return axios.get(weatherUrl);
+}).then((response) => {
+  const temparature = response.data.currently.temperature;
+  const apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${temparature}. It feels like ${apparentTemperature}.`);
+}).catch((error) => {
+  if(error.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API Servers!');
   }
   else {
-    console.log(results.address);
-    weather.getWeather(results.lattitude, results.longitude, (errorMessage, weatherResults) => {
-      if(errorMessage){
-        console.log(errorMessage);
-      }
-      else {
-        console.log(`The temperature is ${weatherResults.temperature}. It feels like actually ${weatherResults.feelsLikeTemperature}.`)
-      }
-    });
+    console.log('Please try again');
   }
 });
